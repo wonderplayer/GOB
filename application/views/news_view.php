593e1580a1a042fc18806e7a7e075969f18da5a9
Rolands Strakis <roland.strakis@gmail.com>
2015-12-16 17:58:25 +0200

    Corrected terrible mistake
    Accidentally moved some lines and view didn't work.

--- application/views/news_view.php
+++ application/views/news_view.php
@@ -1,20 +1,15 @@
-
-
-
-
-
-		<p><?php echo anchor("Main_controller/delete_news/$row->Id" , "Dzēst")?></p>
-		<p><?php echo anchor("Main_controller/goto_update_news/$row->Id" , "Izlabot")?></p>
-	<?php else : ?>
-	<?php endforeach; ?>
-	<?php endif; ?>
-	<?php endif;?>
-	<?php if($this->session->userdata('Username') == 'admin'): ?> 
-	<div><?php echo $row->Description?></div>
-	<h2 name="title" id="title"><?php echo $row->Title ?></h2>
-	<h2>Nav jaunumu.</h2>
-	<hr>
-	<p><?php echo $row->Date ?></p>
-</div>
-<?php if(isset($records)) : foreach ($records as $row ) : ?>
 <div class="container">
+	<?php if(isset($records)) : foreach ($records as $row ) : ?>
+		<h2 name="title" id="title"><?php echo $row->Title ?></h2>
+		<p><?php echo $row->Date ?></p>
+		<div><?php echo $row->Description?></div>
+		<hr>
+		<?php if($this->session->userdata('Username') == 'admin'): ?> 
+			<p><?php echo anchor("Main_controller/delete_news/$row->Id" , "Delete")?></p>
+			<p><?php echo anchor("Main_controller/goto_update_news/$row->Id" , "Update")?></p>
+		<?php endif;?>
+		<?php endforeach; ?>
+		<?php else : ?>
+		<h2>Nav jaunumu.</h2>
+		<?php endif; ?>
+</div>
